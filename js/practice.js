51caba60b1ed4d819800430275d3794688a412ef
Lindsay Garner <gar21034n@byui.edu>
2022-09-16 10:38:28 -0600

    start-up

---
+++ js/practice.js
@@ -0,0 +1,17 @@
+// SEQUENCE 
+
+let date = new Date();
+
+console.log(date);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+document.querySelector('h1').textContent = year;
+
+let currentDate = document.lastModified;
+
+console.log(currentDate);
+
+document.querySelector('div').textContent = currentDate;
