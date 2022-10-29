3eb4407d032ae5f8ffbd6de967102447f9b3c2e1
Lindsay Garner <gar21034n@byui.edu>
2022-10-29 11:22:29 -0600

    discover page

--- chamber/js/lastVisit.js
+++ chamber/js/lastVisit.js
@@ -1,5 +1,28 @@
-let current = Date.now();
-let previous = null;
-let difference = current - previous;
-//sec, days, hours, day
-let diffDays = difference / 1000 / 60 / 60 / 24;
+// local storage
+
+localStorage.setItem("msg1", "Hello Permanent");
+
+sessionStorage.setItem("vsitDate", Date.now());
+let message = localStorage.getItem("msg1");
+let visitDate = localStorage.getItem("visitDate");
+
+console.log(visitDate);
+
+if (!localStorage.getItem("lastVisit")) {
+  localStorage.setItem("lastVisit", Date.now());
+  document.getElementById("diff").textContent = "This is your first visit";
+} else {
+  setStyles();
+}
+
+function setStyles() {
+  let prevDate = localStorage.getItem("lastVisit");
+  let currDate = Date.now();
+
+  let difference = currDate - prevDate;
+  console.log(difference);
+  let daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
+
+  document.getElementById("diff").textContent = daysDifference;
+  localStorage.getItem("lastVisit", Date.now());
+}
--- lazyLoad/js/lazyLoad.js
+++ lazyLoad/js/lazyLoad.js
@@ -1,16 +1,28 @@
 const image = document.querySelectorAll("img");
 
-const pimages = document.querySelectorAll("[data-src]");
+// const io = new IntersectionObserver (
+// (entries, io) => {
+// entries.forEach(entry => {
+// console.log(entries);
+// })
+// },
+// {
+// threshold: [.5]
+// }
+// );
 
+// image.forEach(image => {
+// io.observe(image);
+// })
+
+const pimages = document.querySelectorAll("[data-src]");
 const options = {
   threshold: 1,
-
   rootMargin: "0px 0px 100px 0px",
 };
 
 function preloadImage(img) {
   const source = img.getAttribute("data-src");
-
   if (!source) {
     return;
   }
@@ -18,24 +30,18 @@ function preloadImage(img) {
   img.src = source;
 }
 
-const io = new IntersectionObserver(
-  (entries, io) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        preloadImage(entry.target);
-
-        io.unobserve(entry.target);
-      }
-    });
-
-    // console.log(io);
-  },
-
-  options
-);
+const io = new IntersectionObserver((entries, io) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      io.unobserve(entry.target);
+    }
+  });
+}, options);
 
 pimages.forEach((image) => {
   io.observe(image);
 });
+  
